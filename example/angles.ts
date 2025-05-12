4c0222475427bd75aad4a417d001b2e16bf31f14
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-05-12 22:29:01 +0200

    fix

--- example/angles.ts
+++ example/angles.ts
@@ -4,57 +4,57 @@ import { ActivationFunction, NeuralNetwork } from "../src/nn.ts";
 // output is the quadrant the angle
 
 const nn = new NeuralNetwork(
-  {
-    inputSize: 1,
-    hiddenSize: 1,
-    outputSize: 1,
-    learningRate: 0.01,
-    activationFunction: ActivationFunction.SIGMOID,
-  },
-  ActivationFunction.SIGMOID,
+	{
+		inputSize: 1,
+		hiddenSize: 1,
+		outputSize: 1,
+		learningRate: 0.01,
+		activationFunction: ActivationFunction.SIGMOID,
+	},
+	ActivationFunction.SIGMOID,
 );
 
 const trainingData = [] as { input: number[]; output: number[] }[];
 
 for (let i = 0; i < 360; i++) {
-  const input = [i];
-  const output = [Math.floor(i / 90)];
-  trainingData.push({ input, output });
+	const input = [i];
+	const output = [Math.floor(i / 90)];
+	trainingData.push({ input, output });
 }
 
 const validationData = [] as { input: number[]; output: number[] }[];
 
 for (let i = 0; i < 360; i++) {
-  const input = [i];
-  const output = [Math.floor(i / 90)];
-  validationData.push({ input, output });
+	const input = [i];
+	const output = [Math.floor(i / 90)];
+	validationData.push({ input, output });
 }
 
 nn.train(
-  trainingData.map(({ input, output }) => [input, output]),
-  validationData.map(({ input, output }) => [input, output]),
-  100000,
+	trainingData.map(({ input, output }) => [input, output]),
+	validationData.map(({ input, output }) => [input, output]),
+	100000,
 );
 
 // calculate the accuracy in %
 
 let accuracy = 0;
 
-validationData.forEach(({ input, output }) => {
-  const prediction = nn.feedforward(input)[0];
-  const roundedPrediction = Math.round(prediction);
-  if (roundedPrediction === output[0]) {
-    accuracy++;
-  }
-});
+for (const { input, output } of validationData) {
+	const prediction = nn.feedforward(input)[0];
+	const roundedPrediction = Math.round(prediction);
+	if (roundedPrediction === output[0]) {
+		accuracy++;
+	}
+}
 
 accuracy /= validationData.length;
 
 console.log(`Accuracy: ${accuracy * 100}%`);
 
-validationData.forEach(({ input, output }) => {
-  const prediction = nn.feedforward(input)[0];
-  console.log(
-    `Input: ${input[0]} Output: ${output[0]} Prediction: ${prediction}`,
-  );
-});
+for (const { input, output } of validationData) {
+	const prediction = nn.feedforward(input)[0];
+	console.log(
+		`Input: ${input[0]} Output: ${output[0]} Prediction: ${prediction}`,
+	);
+}
